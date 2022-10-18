48e28d5983a5afa2eeaf741d0c37ce398fc5dad4
Kelvin Crepaldi <kelvin.crepaldi@hotmail.com>
2022-10-18 01:46:36 -0300

    fix: header access control allow origin

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use("/user", routes);
-app.all('/*', function(_, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-});
+})
 
 
 app.listen(port, ()=>console.log(`App running: port ${port}...`));
