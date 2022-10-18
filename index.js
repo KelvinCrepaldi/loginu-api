01371a09247331e608f4665863bbd2276567e085
Kelvin Crepaldi <kelvin.crepaldi@hotmail.com>
2022-10-17 22:12:27 -0300

    feat: added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import routes from "./src/routes";
+import * as cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use("/user", routes);
+app.use (cors());
 
 app.listen(port, ()=>console.log(`App running: port ${port}...`));
